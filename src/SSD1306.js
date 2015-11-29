c95a10e153221a87abbb96fafef697fe8f8eafe9
FlippieCoetser <flippie.coetser@gmail.com>
2015-11-29 10:02:53 +0100

    Add Alpha Tag

---
+++ src/SSD1306.js
@@ -0,0 +1,6 @@
+var SSD1306;
+(function (SSD1306) {
+    var SETCONTRAST = 0x81;
+    var DISPLAYALLON = 0xA5;
+    var NORMALDISPLAY = 0xA6;
+})(SSD1306 || (SSD1306 = {}));
---
+++ src/SSD1306.ts
@@ -0,0 +1,5 @@
+namespace SSD1306 {
+	const SETCONTRAST = 0x81;
+	const DISPLAYALLON = 0xA5;
+	const NORMALDISPLAY = 0xA6;
+}
